4291fb57140779947cb15843b2f66f3d2d44e263
Pedro Marchetti Casteli <pedrocasteli68@gmail.com>
2026-03-13 14:34:46 -0300

    add a modal component

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,18 @@
+const ResultModal = ({ result, targetTime }) => {
+  return (
+    <dialog className="result-modal">
+      <h2>You {result}</h2>
+      <p>
+        The target time was <strong>{targetTime}</strong> seconds.
+      </p>
+      <p>
+        You stopped the timer with <strong>X seconds left.</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+};
+
+export default ResultModal;
